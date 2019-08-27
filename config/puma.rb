cfaecab2945e9a9d2bc083e40e92297498676645
Ginnie Hench <vhench@rti.org>
2019-08-27 19:20:51 -0400

    Dev env settings when using ES instance hosted in cloud.gov - Never commit to master
    - In gemfile, allow pry to be accessed in production mode - this is necessary when using data from https://csapi-test.app.cloud.gov
    - In puma.rb, extend length of time for worker timeout to improve experience with pry

--- config/puma.rb
+++ config/puma.rb
@@ -1,7 +1,7 @@
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['MAX_THREADS'] || 5)
 threads threads_count, threads_count
-worker_timeout 30
+worker_timeout 3600
  
 preload_app!
  
